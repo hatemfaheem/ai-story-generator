9f8693476ab441c4c076f9fe84065a54aff88ab2
Hatem Elseidy <hatem.faheem@gmail.com>
2023-01-15 17:50:22 +0000

    Update image generation prompt for better images

--- generators/story_content_generator.py
+++ generators/story_content_generator.py
@@ -37,8 +37,8 @@ class StoryContentGenerator:
 
         for i in range(len(processed_sentences)):
             image_prompt = (
-                f"Generate art for {story_seed_prompt} '{processed_sentences[i]}'"
-                f" as a children's book illustration."
+                f"A painting for '{processed_sentences[i]}'. "
+                f"{story_seed_prompt}."
             )
             url = self.image_generator.generate_image(
                 prompt=image_prompt, story_size=story_size
